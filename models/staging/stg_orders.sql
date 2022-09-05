eb8603692807c3917bb49e54905fe61e80538047
Haris.Hasanovic@endava.com <Haris.Hasanovic@endava.com>
2022-09-05 10:26:14 +0000

    added delivery_team seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select 
 --from raw orders
+{{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid','p.productid']) }} AS sk_orders,
 o.ORDERID, 
 o.ORDERDATE,
 o.SHIPDATE,
@@ -17,7 +18,9 @@ p.PRODUCTID,
 p.CATEGORY,
 p.PRODUCTNAME,
 p.SUBCATEGORY,
-{{ markup('ordersellingprice', 'ordercostprice') }} AS markup
+{{ markup('ordersellingprice', 'ordercostprice') }} AS markup,
+d.DELIVERY_TEAM
 FROM {{ ref('raw_orders') }} AS o
 LEFT JOIN {{ ref('raw_customers') }} AS c ON o.CUSTOMERID = c.CUSTOMERID
 LEFT JOIN {{ ref('raw_products') }} AS p ON o.PRODUCTID = p.PRODUCTID
+LEFT JOIN {{ ref('delivery_team') }} AS d ON o.SHIPMODE = d.SHIPMODE
